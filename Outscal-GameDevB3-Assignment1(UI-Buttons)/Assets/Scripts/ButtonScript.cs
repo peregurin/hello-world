bdc230dd3a0a33bdb9844ebbd3edb512d32b9093
Peregurin <chinmayvirmani@gmail.com>
2020-03-13 21:12:23 +0530

    completed the solution

---
+++ Outscal-GameDevB3-Assignment1(UI-Buttons)/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    //public GameObject button;
+    //Button button;
+    int sceneIndex;
+
+    private void Start()
+    {
+        sceneIndex = int.Parse(gameObject.name);
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log(gameObject.name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + sceneIndex);
+        //Debug.Log("Button pressed, go to level 01");
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
